3652f9093bf1d2fd4f3d0d35ffd106b3c2e2445c
Mohammadreza <122718610+Cube736@users.noreply.github.com>
2024-01-21 00:39:43 +0330

    test in account app & pages app has wroted by mohammad

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from django.urls import reverse
 
 # Create your tests here.
+
+class SignUpTest(TestCase):
+
+    def test_signup_url_byname(self):
+        response = self.client.get(reverse('signup'))
+        self.assertEqual(response.status_code, 200)
+
+
+    def test_signup_url_byurl(self):
+        response = self.client.get('/accounts/signup')
+        self.assertEqual(response.status_code, 200)
+
+
+
--- pages/urls.py
+++ pages/urls.py
@@ -3,7 +3,7 @@ from django.views.generic.base import TemplateView
 from . import views
 
 urlpatterns = [
-    path('home/', views.HomePageView.as_view(), name = 'home'),
+    path('', views.HomePageView.as_view(), name = 'home'),
     path('contactus/', views.contactusPageView.as_view(), name = 'contactus'),
     # path('about-us/', views.aboutUsview.as_view(), name = 'about-us')
 ]
