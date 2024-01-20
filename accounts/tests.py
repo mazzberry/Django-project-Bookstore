2b8bfb233acb2c994a3bf2e066bc80606da7da79
Mohammadreza <122718610+Cube736@users.noreply.github.com>
2024-01-21 01:37:40 +0330

    test in accounts app has finished

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,9 +1,12 @@
 from django.test import TestCase
 from django.urls import reverse
+from django.contrib.auth import get_user_model
 
 # Create your tests here.
 
 class SignUpTest(TestCase):
+    username = 'myUsername'
+    email = 'myEmail736@gmail.com'
 
     def test_signup_url_byname(self):
         response = self.client.get(reverse('signup'))
@@ -14,5 +17,15 @@ class SignUpTest(TestCase):
         response = self.client.get('/accounts/signup')
         self.assertEqual(response.status_code, 200)
 
+    def test_signup_form(self):
+        #get_user_model() #user modeli ke dakhel project hast ra be ma mide (farghe ba import kardan model:hich moghe jash tagir nemikone)
+        userModel = get_user_model()
+        user = userModel.objects.create_user(
+            self.username,
+            self.email,
+            )
+        self.assertEqual(userModel.objects.all().count(), 1)
+        # self.assertEqual(userModel.objects.all()[0], self.username)
+        # self.assertEqual(userModel.objects.all()[1], self.email)
 
 
