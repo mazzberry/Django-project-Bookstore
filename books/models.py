a6a76fe1adcf02da4bd44ff0a111467d585d97a0
Mohammadreza <122718610+Cube736@users.noreply.github.com>
2024-01-26 06:34:28 +0330

    create books model, books model has registered in admin

--- books/models.py
+++ books/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class books(models.Model):
+
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=5,decimal_places=2)# adad gheymat (XXXXX.XX = 2 ashar, 5 adad)
+
+    def __str__(self):
+        return self.title
