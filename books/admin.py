e1d9de4c5effa16a83162f6068372b4292b51905
Mohammadreza <122718610+Cube736@users.noreply.github.com>
2024-02-10 14:06:47 +0330

    add CommentAdmin class to admin.py in books app to show in sort & clear way

--- books/admin.py
+++ books/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 
-from .models import books
+from .models import books, comments
+@admin.register(comments)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('user', 'text', 'book', 'datetime_created',)
+
 
 admin.site.register(books)
+# admin.site.register(comments)
