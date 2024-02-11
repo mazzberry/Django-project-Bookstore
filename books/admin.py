dcfdb634829f618464aa65d08d265be2c726f8c3
Mohammadreza <122718610+Cube736@users.noreply.github.com>
2024-02-12 00:57:21 +0330

    add is_active & recommend to list_display in commentAdmin

--- books/admin.py
+++ books/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from .models import books, comments
 @admin.register(comments)
 class CommentAdmin(admin.ModelAdmin):
-    list_display = ('user', 'text', 'book', 'datetime_created',)
+    list_display = ('user', 'text', 'book', 'datetime_created','is_active','recommend',)
 
 
 admin.site.register(books)
